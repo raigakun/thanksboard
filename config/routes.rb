91e68871201742be698ccfbdb0b0f4ab5ab88434
raigakun <masashiyoshida2017@gmail.com>
2020-04-22 13:16:03 +0900

    いいねカウント数の登録および表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
 
-  resources :messages,  only: [:new, :create, :show,  :edit, :destroy] 
-  resources :producers,  only: [:index]
-  # do
-  #   resources :likes, only: [:create, :destroy]
-  # end
-  # namespace :admin do
-  #   resources :messages, only: [:index, :new, :create, :show,  :edit, :destroy]
-  # end
-
-  # post "likes/:message_id/create", to: "likes#create", constraints: {message_id: /\d+/}, as: :likes_create
-  # post "likes/:message_id/delete", to: "likes#delete", constraints: {message_id: /\d+/}, as: :likes_delete
+  resources :messages,  only: [:new, :create] do
+    resources :likes, only: :create
+  end
+  
+  resources :producers,  only: :index
+  
 end
--- db/migrate/20200417034659_create_messages.rb
+++ db/migrate/20200417034659_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.2]
   def change
     create_table :messages do |t|
-      t.integer :thx_id
+      t.integer :likes_count
       t.timestamps
     end
   end
